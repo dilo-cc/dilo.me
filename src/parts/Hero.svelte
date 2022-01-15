e989e77e91689909f6edce0a9c802f62da493905
jonathanmv <jonathan.morales@jimdo.com>
2022-01-15 08:51:31 +0100

    added hero background

--- src/parts/Hero.svelte
+++ src/parts/Hero.svelte
@@ -1,4 +1,4 @@
-<section class="hero is-link is-fullheight-with-navbar">
+<section class="hero is-link is-fullheight-with-navbar pattern">
   <div class="hero-body">
     <div class="columns">
       <div class="column is-two-thirds">
@@ -28,11 +28,18 @@
 </section>
 
 <style>
+  .pattern {
+    background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100%25' height='100%25' viewBox='0 0 1600 800'%3E%3Cg %3E%3Cpolygon fill='%23526075' points='1600 160 0 460 0 350 1600 50'/%3E%3Cpolygon fill='%235c6a80' points='1600 260 0 560 0 450 1600 150'/%3E%3Cpolygon fill='%2365748a' points='1600 360 0 660 0 550 1600 250'/%3E%3Cpolygon fill='%236f7e95' points='1600 460 0 760 0 650 1600 350'/%3E%3Cpolygon fill='%237888A0' points='1600 800 0 800 0 750 1600 450'/%3E%3C/g%3E%3C/svg%3E");
+    background-attachment: fixed;
+    background-size: cover;
+   /* background by SVGBackgrounds.com */
+
+  }
   div.video-wrapper {
     padding-top: 10px;
     display: block;
     margin: 0 auto;
-    width: 206px;
+    width: 250px;
   }
   video {
     border-radius: 20px;
