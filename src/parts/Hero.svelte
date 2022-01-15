e6830f005da6257253631bde4829a17c3e398c27
jonathanmv <jonathan.morales@jimdo.com>
2022-01-15 08:14:22 +0100

    added video to hero

--- src/parts/Hero.svelte
+++ src/parts/Hero.svelte
@@ -1,12 +1,55 @@
 <section class="hero is-link is-fullheight-with-navbar">
   <div class="hero-body">
-    <div>
-      <h1 class="title is-1 is-size-2-mobile is-spaced">
-        what are you doing tonight?
-      </h1>
-      <p class="subtitle">
-        dilo finds others who want to do the same, so you don't hang out alone.
-      </p>
+    <div class="columns">
+      <div class="column is-two-thirds">
+        <h1 class="title is-1 is-size-2-mobile is-spaced">
+          what are you doing tonight?
+        </h1>
+        <p class="subtitle">
+          dilo finds others who want to do the same, so you don't hang out
+          alone.
+        </p>
+      </div>
+      <div class="column">
+        <div class="notificationss">
+          <div class="video-wrapper">
+            <video muted autoplay loop>
+              <track kind="captions" />
+              <source src="dinner.mp4" type="video/mp4" />
+            </video>
+          </div>     
+          <div class="frame">
+            <img src="iphone13-frame.png" alt="iphone 13 frame" />
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </section>
+
+<style>
+  div.video-wrapper {
+    padding-top: 10px;
+    display: block;
+    margin: 0 auto;
+    width: 206px;
+  }
+  video {
+    border-radius: 20px;
+  }
+  div.notificationss {
+    width: 300px;
+    position: relative;
+  }
+  div.frame {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    text-align: center;
+  }
+  img {
+    height: 100%;
+  }
+</style>
