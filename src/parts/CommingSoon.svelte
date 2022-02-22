fb878ba8f01f8d385e22b7843b4fba4bef49b83a
jonathanmv <jonathan.morales@jimdo.com>
2022-02-22 01:07:08 +0100

    added links to download dilo

--- src/parts/CommingSoon.svelte
+++ src/parts/CommingSoon.svelte
@@ -9,12 +9,12 @@
 <section class="section p-0 m-0">
   <div class="notification p-0 m-0">
     <iframe
-    title="request early access to dilo"
-    src="https://dilo.substack.com/embed"
-    width="360"
-    height="100"
-    frameborder="0"
-    scrolling="no"
+      title="request early access to dilo"
+      src="https://dilo.substack.com/embed"
+      width="360"
+      height="100"
+      frameborder="0"
+      scrolling="no"
     />
   </div>
 </section>
@@ -22,25 +22,27 @@
   <div class="level">
     <div class="level-item has-text-centered">
       <div>
-        <p class="heading">Available in february</p>
         <div class="notification is-primary">
-          <img
-            width="200"
-            src="https://d1pnnwteuly8z3.cloudfront.net/images/dafc1e05-b0e8-4c6d-b375-4a62333bbd5a/00f3d09f-0db5-49ce-b063-159e4ec36a58.svg"
-            alt="apple store logo"
-          />
+          <a href="https://apps.apple.com/us/app/dilo/id1574622995">
+            <img
+              width="200"
+              src="https://d1pnnwteuly8z3.cloudfront.net/images/dafc1e05-b0e8-4c6d-b375-4a62333bbd5a/00f3d09f-0db5-49ce-b063-159e4ec36a58.svg"
+              alt="apple store logo"
+            />
+          </a>
         </div>
       </div>
     </div>
     <div class="level-item has-text-centered">
       <div>
-        <p class="heading">Available in february</p>
         <div class="notification is-primary">
-          <img
-            width="200"
-            src="https://d1pnnwteuly8z3.cloudfront.net/images/dafc1e05-b0e8-4c6d-b375-4a62333bbd5a/9b92921f-27ed-4beb-831b-e91058b0cdfb.svg"
-            alt="google play logo"
-          />
+          <a href="https://play.google.com/store/apps/details?id=chat.dilo">
+            <img
+              width="200"
+              src="https://d1pnnwteuly8z3.cloudfront.net/images/dafc1e05-b0e8-4c6d-b375-4a62333bbd5a/9b92921f-27ed-4beb-831b-e91058b0cdfb.svg"
+              alt="google play logo"
+            />
+          </a>
         </div>
       </div>
     </div>
